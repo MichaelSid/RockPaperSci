7ae7e35c50bb835d1fe70d7fb6e0bd3e82a017ea
Michael <michael.sidon@gmail.com>
2014-03-31 14:05:46 +0100

    Enabled the winner function to recognize draws & winners

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,3 +1,13 @@
+
+var Pairs = {
+rock: "scissors",
+scissors: "paper",
+paper: "rock"
+}
+
+
+
+
 function Player(name) {
   this.name = name;
 }
@@ -11,6 +21,17 @@ function Game(player1, player2) {
   this.player2 = player2;
 }
 
+// function samePick() {
+// 	this.player1.picks === this.player2.picks;
+// }
+
 Game.prototype.winner = function() {
-  return this.player1;
+	if(this.player1.pick === this.player2.pick) {
+		return null;
+  } else if(Pairs[this.player1.pick] === this.player2.pick) {
+  	return this.player1;
+  } else {
+  	return this.player2;
+  }
 }
+
